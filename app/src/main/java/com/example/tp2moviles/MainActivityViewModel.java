d4974d605b17757b14b96875268f992c06f85f05
NelsonValentinGarroDadan <nelsonvgarro@sanluis.edu.ar>
2023-08-23 18:25:41 -0300

    final

--- app/src/main/java/com/example/tp2moviles/MainActivityViewModel.java
+++ app/src/main/java/com/example/tp2moviles/MainActivityViewModel.java
@@ -1,6 +1,8 @@
 package com.example.tp2moviles;
 
 import android.app.Application;
+import android.content.Context;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -14,14 +16,19 @@ import java.util.List;
 public class MainActivityViewModel extends AndroidViewModel {
     private MutableLiveData<Moneda> monedaM;
     private ArrayList<Moneda> monedas = new ArrayList<Moneda>();
-
+    private Context context;
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
+        context=application.getApplicationContext();
         monedas.add(new Moneda("Euros",(float)1.09,0));
         monedas.add(new Moneda("Dolares",(float)0.92,0));
     }
     public void Convertir(String moneda,double importer){
+        if(moneda.equals("")){
+            Toast.makeText(context, "Debe ingresar algun valor", Toast.LENGTH_LONG).show();
+            return;
+        }
         for(int i=0;i<monedas.size();i++){
             if(moneda.equals(monedas.get(i).getNombre())){
                 double valor = importer * monedas.get(i).getValor();
